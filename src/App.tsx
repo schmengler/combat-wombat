1550c2a4e04a59974b4fb8177056d21e268424fe
Fabian Schmengler <fs@integer-net.de>
2022-02-08 20:29:20 +0100

    Initialize with Vite,React,TypeScript,Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
